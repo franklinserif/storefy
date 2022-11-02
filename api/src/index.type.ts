8984a0b122e0abf56747acf3e7802b5a7e98b224
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-01 21:52:20 -0400

    add IUser interface :label:

--- api/src/index.type.ts
+++ api/src/index.type.ts
@@ -1,1 +1,18 @@
 export type UserRolesTypes = "customer" | "seller";
+
+export interface IUser {
+  id: string;
+  firstName: String;
+  lastName: String;
+  email: String;
+  phoneNumber: String;
+  password: String;
+  roles: "admin" | "seller" | "client";
+  isActive: Boolean;
+  streetNumber: Number;
+  addressLine1: String;
+  addressLine2: String;
+  city: String;
+  region: String;
+  postalCode: Number;
+}
