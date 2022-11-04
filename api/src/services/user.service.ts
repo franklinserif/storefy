aa3e43fb452a3a91d28a8ee31b1022d921263d64
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-04 19:07:57 -0400

    add user services complete :sparkles:

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -4,27 +4,94 @@
  */
 import { User } from "db/entity/User";
 import { IUser } from "index.type";
-import { Boom } from "@hapi/boom";
+import boom from "@hapi/boom";
 import bcrypt from "bcrypt";
 
 export default class UserService {
   /**
-   * insert a new record into the db in the user's table
+   * Insert a new record into the db in the user's table
    * @async
    * @param { Omit<IUser, "id">}  data
-   * @returns {Promise<>}
+   * @returns {Promise<IUser>}
    */
   async create(data: Omit<IUser, "id">) {
     // Encrypt user password
     const encryptedPassword = await bcrypt.hash(data.password, 10);
 
-    /**
-     * Create user instance and merge data with typeORM user instance
-     */
-    const newUser = {
-      ...new User(),
-      ...data,
-      password: encryptedPassword,
-    };
+    const userData = { ...data, password: encryptedPassword };
+
+    const user = await User.create(userData);
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+
+  /**
+   * Find all user in the db
+   * @async
+   * @returns {Promise<IUser[]>}
+   */
+  async findAll() {
+    const users = await User.find();
+
+    return users;
+  }
+
+  /**
+   * Find user by id and return user data with all fields
+   * @async
+   * @param {string} id user id
+   * @returns {Promise<IUser>}
+   */
+  async findOne(id: string) {
+    const user = await User.findOneBy({ id });
+
+    if (!user) throw boom.notFound();
+
+    return user;
+  }
+
+  /**
+   * Find user by email address withou password
+   * @async
+   * @param {string} email email address
+   * @returns {Promise<Iuser>}
+   */
+  async findByEmail(email: string) {
+    const user = await User.findOneBy({ email });
+
+    if (!user) throw boom.notFound();
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+
+  /**
+   * Update user data in database
+   * @async
+   * @param {string} id user id
+   * @param {Partial<IUser>} user data
+   * @returns {Promise<IUser>}
+   */
+  async update(id: string, user: Partial<IUser>) {
+    const updatedUser = await User.update(id, user);
+
+    if (!updatedUser) throw boom.notFound();
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+
+  /**
+   * Remove user data from db
+   * @async
+   * @param {string} id user id
+   *
+   */
+  async delete(id: string) {
+    const user = await this.findOne(id);
+
+    if (!user) throw boom.notFound();
+
+    user.remove();
+
+    return { message: "user was delete from db" };
   }
 }
