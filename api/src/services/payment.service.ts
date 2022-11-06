b9a6d886207f86c2bd549ba9426940157cd7c8af
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-06 02:39:51 -0400

    improve payment business logic :necktie:

--- api/src/services/payment.service.ts
+++ api/src/services/payment.service.ts
@@ -6,16 +6,29 @@
 import { Payment } from "../db/entity/Payment";
 import { IPayment } from "../index.type";
 import boom from "@hapi/boom";
+import UserService from "./user.service";
+
+/**
+ * User service for crud operations
+ * @const
+ * @type {UserService}
+ */
+const userService = new UserService();
 
 export default class PaymentService {
   /**
    * Create a payment
    * @async
+   * @param {string} userId
    * @param {Omit<IPayment, "id">}
    * @returns {Promise<IPayment>}
    */
-  async create(data: Omit<IPayment, "id">) {
+  async create(userId: string, data: Omit<IPayment, "id">) {
+    const user = await userService.findOne(userId);
     const payment = await Payment.create(data);
+
+    payment.user = user;
+    payment.save();
 
     return payment;
   }
