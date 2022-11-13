5b8c704eada33eb628312601b5ad8604d0ee4b22
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-13 17:56:23 -0400

    add new Entity ProductModel :building_construction:

---
+++ api/src/db/entity/ProductModel.ts
@@ -0,0 +1,51 @@
+/**
+ * Module of the entity Product
+ * @module entity/Product
+ */
+
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  OneToMany,
+  ManyToOne,
+  BaseEntity,
+} from "typeorm";
+
+import { Product } from "./Product";
+import { ShoppingCartItem } from "./ShoppingCartItem";
+
+@Entity()
+export class ProductModel extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  qty: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  sizes: string[];
+
+  @Column()
+  colors: string[];
+
+  @ManyToOne(() => Product, (product) => product.productsModels)
+  product: Product;
+
+  @OneToMany(
+    () => ShoppingCartItem,
+    (shoppingCartItem) => shoppingCartItem.product
+  )
+  shoppingCartItems: ShoppingCartItem[];
+
+  @CreateDateColumn({ name: "create_at" })
+  createAt: Date;
+
+  @UpdateDateColumn({ name: "update_at" })
+  updateAt: Date;
+}
