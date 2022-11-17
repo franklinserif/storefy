d20d0e6f137b0391491494734db8c02a1f33ac14
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-16 22:05:45 -0400

    add new entity Variation

---
+++ api/src/db/entity/Variation.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToOne,
+  OneToMany,
+  Entity,
+} from "typeorm";
+import { ProductModel } from "./ProductModel";
+import { VariationOption } from "./VariationOptions";
+
+@Entity()
+export class Variation extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => ProductModel, (productModel) => productModel.variations)
+  productModel: ProductModel;
+
+  @OneToMany(
+    () => VariationOption,
+    (variationOption) => variationOption.variation
+  )
+  variationOptions: VariationOption[];
+}
