f6b71ead03ee29630fd2c291cf25a7598e610875
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-19 13:58:53 -0400

    update removePassword function docs :memo:

--- api/src/utils/removePassword.ts
+++ api/src/utils/removePassword.ts
@@ -2,7 +2,8 @@ import { User } from "../db/entity/User";
 
 /**
  * remove password from User object
- * @param {User} user
+ * @param user
+ * @returns user without password
  */
 export default function removePassword(user: User) {
   const { password, ...userWithoutPassword } = user;
