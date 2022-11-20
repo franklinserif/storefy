4a85951e30599b7bfb693c9f6377529f3eeb5b40
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-20 19:06:03 -0400

    remove type docs

--- api/src/schemas/shoppingCartSchemas.ts
+++ api/src/schemas/shoppingCartSchemas.ts
@@ -9,21 +9,18 @@ import { IShoppingCart } from "../index.type";
 /**
  * shoppingCart id uuid
  * @const
- * @type {joi.StringSchema<string>}
  */
 const id = joi.string().uuid();
 
 /**
  * shoppingCart id uuid
  * @const
- * @type {joi.StringSchema<number>}
  */
 const total = joi.number();
 
 /**
  * shoppingcart create schema validation
  * @const
- * @type {joi.ObjectSchema<IShoppingCart>}
  */
 export const shoppingCartCreateSchema = joi.object<IShoppingCart>({
   total: total.required(),
@@ -32,7 +29,6 @@ export const shoppingCartCreateSchema = joi.object<IShoppingCart>({
 /**
  * shoppingcart update schema validation
  * @const
- * @type {joi.ObjectSchema<IShoppingCart>}
  */
 export const shoppingCartUpdateSchema = joi.object<IShoppingCart>({
   total,
@@ -41,7 +37,6 @@ export const shoppingCartUpdateSchema = joi.object<IShoppingCart>({
 /**
  * shoppingcart id schema validation
  * @const
- * @type {joi.ObjectSchema<IShoppingCart>}
  */
 export const shoppingCartIdSchema = joi.object<IShoppingCart>({
   id: id.required(),
