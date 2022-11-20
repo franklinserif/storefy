63e717d57d45ef0753e542878cbc97563e2116db
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-20 19:05:54 -0400

    remove type docs

--- api/src/schemas/categorySchemas.ts
+++ api/src/schemas/categorySchemas.ts
@@ -9,28 +9,24 @@ import { ICategory, IAddOrRemoveCategoryParent } from "../index.type";
 /**
  * category id uuid
  * @const
- * @type {joi.StringSchema<string>}
  */
 const id = joi.string().uuid();
 
 /**
  * category name
  * @const
- * @type {joi.StringSchema<string>}
  */
 const name = joi.string();
 
 /**
  * category image
  * @const
- * @type {joi.StringSchema<string>}
  */
 const image = joi.string();
 
 /**
  * category creation validation schema
  * @const
- * @type {joi.ObjectSchema<ICategory>}
  */
 export const categoryCreateSchema = joi.object<ICategory>({
   name: name.required(),
@@ -40,7 +36,6 @@ export const categoryCreateSchema = joi.object<ICategory>({
 /**
  * category update data validation schema
  * @const
- * @type {joi.ObjectSchema<ICategory>}
  */
 export const categoryUpdateSchema = joi.object<ICategory>({
   name,
@@ -50,7 +45,6 @@ export const categoryUpdateSchema = joi.object<ICategory>({
 /**
  * category id validation schema
  * @const
- * @type {joi.ObjectSchema<ICategory>}
  */
 export const categoryIdSchema = joi.object<ICategory>({
   id: id.required(),
