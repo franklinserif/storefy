e04037e17674184d1e71d7c68048509885273c03
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-20 19:05:59 -0400

    remove type docs

--- api/src/schemas/promotionSchemas.ts
+++ api/src/schemas/promotionSchemas.ts
@@ -9,49 +9,42 @@ import { IPromotion } from "../index.type";
 /**
  * promotion id uuid
  * @const
- * @type {joi.StringSchema<string>}
  */
 const id = joi.string().uuid();
 
 /**
  * promotion name
  * @const
- * @type {joi.StringSchema<string>}
  */
 const name = joi.string();
 
 /**
  * promotion description
  * @const
- * @type {joi.StringSchema<string>}
  */
 const description = joi.string();
 
 /**
  * promotion discount rate
  * @const
- * @type {joi.NumberSchema<number>}
  */
 const discountRate = joi.number();
 
 /**
  * promotion start date
  * @const
- * @type {joi.DateSchema<Date>}
  */
 const startDate = joi.date();
 
 /**
  * promotion end date
  * @const
- * @type {joi.DateSchema<Date>}
  */
 const endDate = joi.date();
 
 /**
  * promotion creation schema validation
  * @const
- * @type {joi.ObjectSchema<IPromotion>}
  */
 export const promotionCreateSchema = joi.object<IPromotion>({
   name: name.required(),
@@ -64,7 +57,6 @@ export const promotionCreateSchema = joi.object<IPromotion>({
 /**
  * promotion update data schema validation
  * @const
- * @type {joi.ObjectSchema<IPromotion>}
  */
 export const promotionUpdateSchema = joi.object<IPromotion>({
   name,
@@ -77,7 +69,6 @@ export const promotionUpdateSchema = joi.object<IPromotion>({
 /**
  * promotion id schema validation
  * @const
- * @type {joi.ObjectSchema<IPromotion>}
  */
 export const promotionIdSchema = joi.object<IPromotion>({
   id: id.required(),
