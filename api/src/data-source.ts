b45789aa5e93b5ffc62cadb27c6955e29855e891
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-25 21:49:01 -0400

    fix import with entity

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./db/entity/User";
-import { Category } from "./db/entity/Category";
-import { Payment } from "./db/entity/Payment";
-import { ProductRating } from "./db/entity/ProductRating";
-import { Promotion } from "./db/entity/Promotion";
-import { Review } from "./db/entity/Review";
-import { ShoppingCart } from "./db/entity/ShoppingCart";
-import { ShoppingCartItem } from "./db/entity/ShoppingCartItem";
-import { Product } from "./db/entity/Product";
-import { ProductModel } from "./db/entity/ProductModel";
-import { WishList } from "./db/entity/WishList";
-import { Variation } from "./db/entity/Variation";
-import { VariationOption } from "./db/entity/VariationOption";
+import { User } from "./db/entity/User.entity";
+import { Category } from "./db/entity/Category.entity";
+import { Payment } from "./db/entity/Payment.entity";
+import { ProductRating } from "./db/entity/ProductRating.entity";
+import { Promotion } from "./db/entity/Promotion.entity";
+import { Review } from "./db/entity/Review.entity";
+import { ShoppingCart } from "./db/entity/ShoppingCart.entity";
+import { ShoppingCartItem } from "./db/entity/ShoppingCartItem.entity";
+import { Product } from "./db/entity/Product.entity";
+import { ProductModel } from "./db/entity/ProductModel.entity";
+import { WishList } from "./db/entity/WishList.entity";
+import { Variation } from "./db/entity/Variation.entity";
+import { VariationOption } from "./db/entity/VariationOption.entity";
 import CONFIG from "./config";
 
 export const AppDataSource = new DataSource({
