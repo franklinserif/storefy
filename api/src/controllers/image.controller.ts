14dbffe8d69bebadd0417b1a750aaa7b6bc82f70
Franklin Rodriguez <franklinserif@gmail.com>
2022-11-26 23:30:14 -0400

    add image controller

---
+++ api/src/controllers/image.controller.ts
@@ -0,0 +1,58 @@
+/**
+ * Image controllers for handler request
+ * @module controllers/imageControllers
+ */
+
+import { Request, Response, NextFunction } from "express";
+import ImageService from "../services/image.service";
+
+/**
+ * Product services for all crud operation in db
+ * @const
+ */
+const imageService = new ImageService();
+
+/**
+ * create category controller
+ * @async
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function addImageToProductController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const image = await imageService.addImageToProduct(id, data);
+
+    res.status(200).json(image);
+  } catch (error) {
+    next(error);
+  }
+}
+
+/**
+ * delete image controller
+ * @async
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function imageDeleteController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+    const rta = await imageService.delete(id);
+
+    res.status(201).json(rta);
+  } catch (error) {
+    next(error);
+  }
+}
