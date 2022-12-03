b6203d5abd468d4387fd3e7bf4b77df55c7652e1
Franklin Rodriguez <franklinserif@gmail.com>
2022-12-03 14:25:01 -0400

    Change variable declaration from var to const

--- api/src/middlewares/uploadFile.handler.ts
+++ api/src/middlewares/uploadFile.handler.ts
@@ -29,7 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   fileFilter: function (_req, file, callback) {
-    var ext = path.extname(file?.originalname);
+    const ext = path.extname(file?.originalname);
     if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
       return callback(new Error("Only images are allowed"));
     }
