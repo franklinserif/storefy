1a3de791d6ed28ab3e16f621cf3760f0ac01a5b9
Franklin Rodriguez <franklinserif@gmail.com>
2022-12-07 18:35:52 -0400

    feat(tailwindcss): :sparkles: add taildwindcss

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+export interface IButton {
+  children: React.ReactNode;
+  type: "PRIMARY" | "SECONDARY";
+  size: "SM" | "MD" | "BG";
+  rounded: "SM" | "MD" | "BG" | "CIRCLE";
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
+}
