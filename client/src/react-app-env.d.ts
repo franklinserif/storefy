2fcf75ceecff4f98486b119e21f94397f7715475
Franklin Rodriguez <franklinserif@gmail.com>
2022-12-07 19:59:41 -0400

    feat(UI): :sparkles: Input component

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,8 +1,20 @@
 /// <reference types="react-scripts" />
+
 export interface IButton {
   children: React.ReactNode;
   type: "PRIMARY" | "SECONDARY";
   size: "SM" | "MD" | "BG";
   rounded: "SM" | "MD" | "BG" | "CIRCLE";
   onClick: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+export interface IInput {
+  name: string;
+  type: "text" | "password" | "email" | "search";
+  size: "SM" | "MD" | "BG";
+  rounded: "SM" | "MD" | "BG" | "CIRCLE";
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  error?: boolean;
+  required?: boolean;
 }
