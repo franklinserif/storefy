4e5bcd542dd00e67a5fc1d130f0e88931fe60b78
Franklin Rodriguez <franklinserif@gmail.com>
2022-12-07 20:01:14 -0400

    style(UI): :lipstick: add style for input

---
+++ client/src/components/Input/style.ts
@@ -0,0 +1,29 @@
+/**
+ * input style
+ */
+
+export const inputBaseStyle =
+  "transition-all text-teal-500 font-weight-medium focus:border-teal-500 outline-none block border-solid border-2";
+
+export const inputSize = {
+  SM: " py-1 px-3 text-xs ",
+  MD: " py-2 px-6 text-sm ",
+  BG: " py-4 px-8 text-md ",
+};
+
+export const labelBaseStyle =
+  "text-inherit font-weight-medium normal-case block mb-2";
+
+export const labelSize = {
+  SM: " text-xs ",
+  MD: " text-sm ",
+  BG: " text-md ",
+};
+
+export const inputRounded = {
+  SM: "rounded ",
+  MD: "rounded-md ",
+  BG: "rounded-lg ",
+  CIRCLE: "rounded-full ",
+};
+export const errorStyle = " border-red-500 ";
