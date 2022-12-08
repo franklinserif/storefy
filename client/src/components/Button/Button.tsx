ca304b85e9423b3e724961ee7a35db9738cd30b6
Franklin Rodriguez <franklinserif@gmail.com>
2022-12-07 20:00:17 -0400

    docs(UI): :memo: docs for button

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { buttonType, buttonSize, buttonRounded } from "./style";
 import { IButton } from "../../react-app-env";
 
+/**
+ * Button component with all differents type, size and shapes
+ * @param props
+ * @param props.children
+ * @param props.onClick
+ * @param props.type
+ * @param props.size
+ * @param props.rounded
+ * @returns JSX.Element
+ */
 const Button: React.FC<IButton> = ({
   children,
   onClick,
